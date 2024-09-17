f0207f3fee4d37cd6991829bd7adda697d745891
Carlos <cadanielmarchesin@gmail.com>
2024-09-16 21:21:12 -0300

    @Component, @Autowired, @SpringBootApplication

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,29 +1,27 @@
 package academy.devdojo.springboot2.controller;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.util.DateUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashSet;
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Set;
 
 @RestController
 @RequestMapping("anime")
+@Log4j2
+@RequiredArgsConstructor
 public class AnimeController {
+    private final DateUtil dateUtil;
+
     //localhost:8080/anime/list
     @GetMapping(path = "list")
     public List<Anime> list() {
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return List.of(new Anime("DBZ"), new Anime("Beserk"));
-    }
-
-    // Fora do curso - só pra testar
-    @GetMapping(path = "set")
-    public Set<Anime> set() {
-        HashSet<Anime> animeHashSet = new HashSet<>();
-        animeHashSet.add(new Anime("Hajime no Ippo"));
-        animeHashSet.add(new Anime("Boku no Hero"));
-        return animeHashSet;
     }
 }
