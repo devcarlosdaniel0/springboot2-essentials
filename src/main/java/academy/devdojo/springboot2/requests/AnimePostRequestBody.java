c9a3794e0d127026b32c4844f71aca5d204b6230
Carlos <cadanielmarchesin@gmail.com>
2024-09-20 18:55:32 -0300

    Validação de campos

--- src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -1,8 +1,10 @@
 package academy.devdojo.springboot2.requests;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class AnimePostRequestBody {
+    @NotEmpty(message = "The name can't be empty")
     private String name;
 }
