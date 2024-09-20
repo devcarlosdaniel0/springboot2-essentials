628eb3af1ee9fbef4c4f10cb51bfd9edaec230a6
Carlos <cadanielmarchesin@gmail.com>
2024-09-20 18:57:32 -0300

    @NotBlank permite que não seja vazio, nulo e nem espaços em branco

--- src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -1,10 +1,10 @@
 package academy.devdojo.springboot2.requests;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class AnimePostRequestBody {
-    @NotEmpty(message = "The name can't be empty")
+    @NotBlank(message = "The name can't be empty")
     private String name;
 }
