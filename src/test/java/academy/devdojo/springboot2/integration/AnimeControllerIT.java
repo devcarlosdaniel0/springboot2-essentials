6cd96063522f13d16cdd9b3663be8b6c1761ff4f
Carlos <cadanielmarchesin@gmail.com>
2024-10-01 17:46:04 -0300

    Create AnimeControllerIT.java

---
+++ src/test/java/academy/devdojo/springboot2/integration/AnimeControllerIT.java
@@ -0,0 +1,8 @@
+package academy.devdojo.springboot2.integration;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AnimeControllerIT {
+    
+}
