8abcd4df5796252eff5e18ce51ca7b73f79170a3
Carlos <cadanielmarchesin@gmail.com>
2024-10-09 18:33:33 -0300

    CSRF Token

--- src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
+++ src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package academy.devdojo.springboot2.config;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -13,22 +15,34 @@ import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@Log4j2
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authz) -> authz
-                .anyRequest().authenticated()
-        ).httpBasic(withDefaults());
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authz) -> authz.anyRequest().authenticated())
+                .httpBasic(withDefaults());
+
         return http.build();
     }
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
         PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        UserDetails user = User.withUsername("devdojo")
+
+        UserDetails user1 = User.withUsername("carlos")
+                .password(encoder.encode("academy"))
+                .roles("USER", "ADMIN")
+                .build();
+
+        UserDetails user2 = User.withUsername("devdojo")
                 .password(encoder.encode("academy"))
                 .roles("USER")
                 .build();
-        return new InMemoryUserDetailsManager(user);
+
+        log.info("password enconder: '{}'", encoder.encode("test"));
+
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 }
